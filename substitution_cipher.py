b2a45e75ada0c0a8031f0aa0045b27372ae940ac
Daine Bigham <Daine.Bigham@datacom.co.nz>
2021-04-30 17:15:13 +1200

    added functionality to decode using the same cipher

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -8,7 +8,7 @@ def substitution_cipher():
         print("USAGE: python encrypt/decrypt substitution.py KEY")
         return 1
     arg1 = sys.argv[1].lower()
-    arg2 = sys.argv[2]
+    arg2 = sys.argv[2].lower()
 
     # check if arguments are valid
     if arg1 == 'encrypt' or arg1 == 'decrypt' or is_valid_key(arg2) is True:
@@ -19,19 +19,36 @@ def substitution_cipher():
 def substitution(code, key):
     # store the alphabet in a variable for easy indexing, create empty cariable to hold coded message, and ask user for message
     alpha = string.ascii_lowercase
-    encoded = ""
 
-    if code == 'encrypt'
+    if code == 'encrypt':
+        encoded = ""
         message = input("Enter message to encrypt: ").lower()
 
         # for each character in the message, find the messages index in the alphabet and store it in temp variable
         for ch in message:
-            alpha_temp = alpha.index(ch)
-            # then create a temp variable to store the letter of the same index in the key variable and add it to the coded message
-            key_temp = key[alpha_temp]
-            encoded += key_temp
+            if ch in alpha:
+                alpha_temp = alpha.index(ch)
+                # then create a temp variable to store the letter of the same index in the key variable and add it to the coded message
+                key_temp = key[alpha_temp]
+                encoded += key_temp
+            else:
+                encoded += ch
 
         print(f"Your coded message is: {encoded}")
+    elif code == 'decrypt':
+        decoded = ""
+        message = input("Enter message to decrypt: ").lower()
+
+        # same as above but in reverse
+        for ch in message:
+            if ch in alpha:
+                key_temp = key.index(ch)
+                alpha_temp = alpha[key_temp]
+                decoded += alpha_temp
+            else:
+                decoded += ch
+        
+        print(f"Your decoded message is: {decoded}")
 
 def is_valid_key(key):
     # count if any characters in the key are repeats and create duplicate check var
