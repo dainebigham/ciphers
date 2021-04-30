66e387598fe2a9593f9dfc054e6f3b320aa10eae
Daine Bigham <Daine.Bigham@datacom.co.nz>
2021-04-30 15:02:46 +1200

    completed caeser cipher functionality

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -34,8 +34,33 @@ def caeser_cipher():
         else:
             print("Incorrect input. Type '(e)ncode' for encryption, or '(d)ecode' for decryption")
 
-def caeser(text, number, code):
+def caeser(text, shift, direction):
     # create a list of lowercase letters to rotate through
     alpha = list(string.ascii_lowercase)
+    coded = ""
+    
+    # loop through each character in the input message
+    for ch in text:
+        # if the character is not in alpha list then just add it to output text
+        if ch not in alpha:
+            coded += ch
+        # if we are encoding message
+        elif direction == 'e' or direction == 'encode':
+            # find the location of that letter in the alpha array and add amount of shift
+            alpha_index = alpha.index(ch) + shift
+            # if this length is greater than the array length, minus by 26 to bring index back in line with alphabet
+            if alpha_index > 25:
+                alpha_index -= 26
+            # add shifted letter to the output string
+            coded += alpha[alpha_index]
+            # else do the same but in reverse
+        else:
+            alpha_index = alpha.index(ch) - shift
+            
+            if alpha_index > 25:
+                alpha_index += 26
+
+            coded += alpha[alpha_index]
+    print(f"Your message is {coded}")
 
 caeser_cipher()
